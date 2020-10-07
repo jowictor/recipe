b6eb1e5541116540584cfe88b272bbe71f34e9f7
joao <joao.silva@allog.com.br>
2020-10-07 03:42:50 -0300

    refactor result getRecipeList

--- src/service/recipeService.ts
+++ src/service/recipeService.ts
@@ -1,20 +1,24 @@
 import { apiBase } from '../serviceFramework/apiBase';
-
+import { puppyApi } from '../externalAPI/puppyApi';
+import { returnModel } from '../model/returnModel';
+import { Status } from '../interface/Status';
 
 export class recipeService extends apiBase {
-   
+
     constructor() {
         super();
     }
 
-    public async getRecipeList(): Promise<any> {
+    public async getRecipeList(recipeList: string[]): Promise<any> {
         try {
-           
-
+            if (!recipeList) return this.error(Status.FAILED, 'recipeList params in getRecipeList is null');
+            const puppyService = new puppyApi();
+            const result: returnModel = await puppyService.getRecipesList(recipeList);
+            if (!result || result.$status != Status.SUCCESS) return this.error(result.$status, result.$message);
+            return this.sucess(result);
         } catch (ex) {
-            console.log(`exception to getRecipeList : ${ex}`)
+            console.log(`exception to getRecipeList : ${ex}`);
             throw this.criticalError(ex.message);
         }
     }
 }
-
